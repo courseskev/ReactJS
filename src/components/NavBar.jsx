483713204dcfe222f6e46e51f3d86a09b1dc58fb
Jachk0011 <kibarra@unal.edu.co>
2022-08-03 09:44:01 -0500

    NavBar with logo and list of categories

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import logo from "../assets/logoML_2.png";
 
 function NavBar() {
-    return (
-        <>
-            <Navbar bg="dark" variant="dark">
-                <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                    </Nav>
-                </Container>
-            </Navbar>
-        </>
-    );
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={logo} width={50} heigth={50} alt="logo" />
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#promotions">Promociones</Nav.Link>
+            <Nav.Link href="#tech">Tecnología</Nav.Link>
+            <Nav.Link href="#clothes">Ropa</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
 
 export default NavBar;

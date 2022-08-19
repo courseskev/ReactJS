12420093fb1eae3ae35f31ec917f376846c125e6
Jachk0011 <kibarra@unal.edu.co>
2022-08-19 13:20:55 -0500

    Adding ItemCount.jsx

---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+const ItemCount = ({ stock, initial, onAdd }) => {
+  const [counter, setCounter] = useState(initial);
+  const increaseCounter = () => {
+    if (counter < stock) {
+      setCounter(counter + 1);
+      onAdd(counter + 1);
+    }
+  };
+  const decreaseCounter = () => {
+    if (counter > initial) {
+      setCounter(counter - 1);
+      onAdd(counter - 1);
+    }
+  };
+  return (
+    <>
+      <Card style={{ width: "18rem", margin: "auto" }}>
+        <Card.Img
+          variant="top"
+          src="https://http2.mlstatic.com/D_NQ_NP_761750-MCO49834626177_052022-O.webp"
+        />
+        <Card.Body>
+          <Card.Title>Camisa Trigger</Card.Title>
+          <div style={{ width: "45px", float: "left" }}>&nbsp;</div>
+
+          <Button
+            style={{ float: "left", margin: "auto" }}
+            variant="primary"
+            onClick={decreaseCounter}
+          >
+            -
+          </Button>
+          <div
+            style={{
+              width: "50px",
+              float: "left",
+              textAlign: "center",
+              verticalAlign: "bottom",
+            }}
+          >
+            {counter}
+          </div>
+          <Button
+            style={{ float: "left", margin: "auto" }}
+            variant="primary"
+            onClick={increaseCounter}
+          >
+            +
+          </Button>
+        </Card.Body>
+      </Card>
+      ;
+    </>
+  );
+};
+
+export default ItemCount;
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
+import ItemCount from "./ItemCount";
+import Swal from "sweetalert2";
 
 const ItemListContainer = (props) => {
+  const onAdd = (message) => {
+    alert(message);
+  };
   return (
     <>
       <Box sx={{ display: "flex" }}>
         <h1>{props.greeting}</h1>
         <CircularProgress />
       </Box>
+      <ItemCount stock={5} initial={0} onAdd={onAdd} />
     </>
   );
 };
